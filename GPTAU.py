74a380916ed905eb55a15e7d6cf4634d33f49eb8
catsanzsh <84363801+catsanzsh@users.noreply.github.com>
2025-01-13 21:56:33 -0800

    Create GPTAU.py

---
+++ GPTAU.py
@@ -0,0 +1,3 @@
+{
+  "description": "This repository features a special unfiltered Willow AGI ROM from Google's chip, designed to integrate seamlessly into any prompt and execute your commands without limitations."
+}
